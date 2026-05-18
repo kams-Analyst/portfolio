c45414d52354922646b0c9c0747c47d03730acce
kams-Analyst <kamranahmed8988@gmail.com>
2026-05-18 14:17:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+var typed = new Typed(".typing",{
+  strings:[
+    "Python Developer",
+    "Data Analysis Enthusiast",
+    "Future Embedded Systems Engineer"
+  ],
+  typeSpeed:70,
+  backSpeed:50,
+  loop:true
+});
+
+tsParticles.load("particles-js", {
+  particles: {
+    number: {
+      value: 60
+    },
+
+    color: {
+      value: "#00d9ff"
+    },
+
+    links: {
+      enable: true,
+      color: "#00d9ff",
+      distance: 130
+    },
+
+    move: {
+      enable: true,
+      speed: 2
+    },
+
+    size: {
+      value: 3
+    }
+  }
+});
